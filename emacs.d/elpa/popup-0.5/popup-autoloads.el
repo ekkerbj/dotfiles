bb4d44801b01ec28ec9c63a8398f45c1c1e49199
Jeff Brekke <ekkerbj@gmail.com>
2014-01-07 21:52:52 -0600

    Added auto-complete.

---
+++ emacs.d/elpa/popup-0.5/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21196 51928
+;;;;;;  649570 561000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
